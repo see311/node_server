631e7c3ed4dabb2c18c444877dd85b2b0cf86c67
see311 <515372622@163.com>
2017-08-08 11:43:51 +0800

    Error handler middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,20 @@ const Ctrl = require('koa-route');
 const app = new Koa();
 const FileManager = require('./module/file_manager.js');
 
+app.use(async (ctx, next) => {
+    try{
+        await next();
+    }catch(e){
+        // console.log(ctx.req)
+        console.log('-------------------------------------------------------------------')
+        console.log(ctx.url);
+        console.log('-------------------------------------------------------------------')
+        console.log(e);
+        ctx.throw(404);
+        ctx.body = e.Error;
+    }
+})
+
 app.use(Ctrl.get('/api/item_list', async (ctx) => {
     ctx.set('Cache-Control', 'no-cache');
     ctx.body = await FileManager.getItemList();
--- server/module/file_manager.js
+++ server/module/file_manager.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = {
     getItemList() {
         return new Promise((res, rej) => {
-            fs.readFile('./user_info.json', 'utf-8', (err, data) => {
+            fs.readFile('../user_info.json', 'utf-8', (err, data) => {
                 err ? rej(err) : res(data);
             })
         })
