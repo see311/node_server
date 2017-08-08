782997eca340e6a8e83be95b195db7ef715def04
see311 <515372622@163.com>
2017-08-08 18:03:59 +0800

    add_item api

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ const Ctrl = require('koa-route');
 const app = new Koa();
 const FileManager = require('./module/file_manager.js');
 
+// 错误处理
 app.use(async (ctx, next) => {
-    try{
+    try {
         await next();
-    }catch(e){
+    } catch (e) {
         // console.log(ctx.req)
         console.log('-------------------------------------------------------------------')
         console.log(ctx.url);
+        console.log(ctx.method);
         console.log('-------------------------------------------------------------------')
         console.log(e);
-        ctx.throw(404);
         ctx.body = e.Error;
     }
 })
@@ -22,6 +23,18 @@ app.use(Ctrl.get('/api/item_list', async (ctx) => {
     ctx.set('Cache-Control', 'no-cache');
     ctx.body = await FileManager.getItemList();
 }))
+
+app.use(Ctrl.get('/api/add_item', async (ctx) => {
+    let query = ctx.query;
+    let name = query.name,
+        phone = query.phone,
+        attend = query.attend;
+    ctx.body = await FileManager.addItem(name, phone, attend);
+}))
+
+app.use(async (ctx) => {
+    ctx.body = '404'
+})
 
 app.listen(port, () => {
     console.log(`server has been started at http://127.0.0.1:${port}`)
--- server/module/file_manager.js
+++ server/module/file_manager.js
@@ -3,8 +3,25 @@ const fs = require('fs');
 module.exports = {
     getItemList() {
         return new Promise((res, rej) => {
-            fs.readFile('../user_info.json', 'utf-8', (err, data) => {
-                err ? rej(err) : res(data);
+            fs.readFile('user_info.json', 'utf-8', (err, data) => {
+                err ? rej(err) : res(JSON.parse(data));
+            })
+        })
+    },
+    addItem(name, phone, attend) {
+        return new Promise((res, rej) => {
+            fs.readFile('user_info.json', 'utf-8', (err, data) => {
+                if (err) {
+                    rej(err)
+                } else {
+                    let data_arr = JSON.parse(data);
+                    let user_info = { name: name, phone: phone, attend: attend };
+                    data_arr.push(user_info);
+                    data_json = JSON.stringify(data_arr);
+                    fs.writeFile('user_info.json',data_json,'utf-8',(err)=>{
+                        err?rej(err):res('写入成功')
+                    })
+                }
             })
         })
     }
