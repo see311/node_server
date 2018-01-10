f03d5acacdc443dc8fe303280ddbf263759e9d8e
wangze <wangze@embway.com>
2018-01-10 16:17:55 +0800

    current time

--- app.js
+++ app.js
@@ -29,17 +29,21 @@ app.enable('trust proxy');
 // app.use(AV.Cloud.HttpsRedirect());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', function (req, res) {
+  res.render('index', {
+    currentTime: new Date()
+  });
 });
 
 // 可以将一类的路由单独保存在一个文件中
 // app.use('/todos', require('./routes/todos'));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
   if (!res.headersSent) {
     var err = new Error('Not Found');
@@ -49,7 +53,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   if (req.timedout && req.headers.upgrade === 'websocket') {
     // 忽略 websocket 的超时
     return;
