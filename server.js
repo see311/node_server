332fb43821f488a9878a5118afac75ef3fe059ac
wangze <wangze@embway.com>
2018-01-10 17:40:39 +0800

    loadingCover

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 var AV = require('leanengine');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  appId: process.env.LEANCLOUD_APP_ID || '000',
+  appKey: process.env.LEANCLOUD_APP_KEY || '000',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || '000'
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
@@ -21,10 +21,10 @@ app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
 
   // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
+  process.on('uncaughtException', function (err) {
     console.error('Caught exception:', err.stack);
   });
-  process.on('unhandledRejection', function(reason, p) {
+  process.on('unhandledRejection', function (reason, p) {
     console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
   });
 });
