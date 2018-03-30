305359b13da4d18dd07b591675d067fe800392b6
wangze <wangze@embway.com>
2018-03-30 17:26:42 +0800

    111111

--- app.js
+++ app.js
@@ -29,20 +29,7 @@ app.enable('trust proxy');
 // 需要重定向到 HTTPS 可去除下一行的注释。
 // app.use(AV.Cloud.HttpsRedirect());
 
-// app.use(bodyParser.xml({
-//   limit: "1MB", // Reject payload bigger than 1 MB
-//   xmlParseOptions: {
-//     normalize: true, // Trim whitespace inside text nodes
-//     normalizeTags: true, // Transform tags to lowercase
-//     explicitArray: false // Only put nodes in array if >1
-//   },
-//   verify: function (req, res, buf, encoding) {
-//     if (buf && buf.length) {
-//       // Store the raw XML
-//       req.rawBody = buf.toString(encoding || "utf8");
-//     }
-//   }
-// }));
+app.use(bodyParser.xml());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
