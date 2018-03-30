22b724a6dad85adcc4a9ba97eab12395d4d2d18c
wangze <wangze@embway.com>
2018-03-30 17:32:57 +0800

    444444

--- app.js
+++ app.js
@@ -30,17 +30,16 @@ app.enable('trust proxy');
 // app.use(AV.Cloud.HttpsRedirect());
 
 app.use(bodyParser.xml({
-  limit: "1MB", // Reject payload bigger than 1 MB
-  // xmlParseOptions: {
-  //   normalize: true, // Trim whitespace inside text nodes
-  //   normalizeTags: true, // Transform tags to lowercase
-  //   explicitArray: false // Only put nodes in array if >1
-  // },
-  // verify: function (req, res, buf, encoding) {
-  //   if (buf && buf.length) {
-  //     // Store the raw XML
-  //     req.rawBody = buf.toString(encoding || "utf8");
-  //   }
+  xmlParseOptions: {
+    normalize: true, // Trim whitespace inside text nodes
+    normalizeTags: true, // Transform tags to lowercase
+    explicitArray: false // Only put nodes in array if >1
+  },
+  verify: function (req, res, buf, encoding) {
+    if (buf && buf.length) {
+      // Store the raw XML
+      req.rawBody = buf.toString(encoding || "utf8");
+    }
   // }
 }));
 app.use(bodyParser.json());
