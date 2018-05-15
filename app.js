87628491f3b33af4e21574415ab83b9e1f6dee56
wangze <wangze@embway.com>
2018-05-15 17:54:14 +0800

    addlj

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'views/personal_web')));
 
 // 设置默认超时时间
 app.use(timeout('15s'));
